7d3c296620ea7387d3567dfc4c97548f5684696f
Sudipta Chowdhury <sudipta.chowdhury@welldev.io>
2024-12-29 14:55:19 +0600

    setup initial wago server

--- wago.js
+++ wago.js
@@ -9,22 +9,6 @@ const server = net.createServer((socket) => {
       data.toString("hex").toUpperCase()
     );
 
-    // Simulate a response for a Modbus TCP request
-    // For example, a typical response to a Modbus read request (Function Code 3):
-    // const response = Buffer.from([
-    //   0x00,
-    //   0x01, // Transaction Identifier (2 bytes)
-    //   0x00,
-    //   0x00, // Protocol Identifier (2 bytes)
-    //   0x00,
-    //   0x06, // Length (2 bytes)
-    //   0x01, // Unit Identifier (1 byte)
-    //   0x03, // Function Code (1 byte)
-    //   0x02, // Byte Count (2 bytes)
-    //   0x00,
-    //   0x01, // Register Value (2 bytes)
-    // ]);
-
     const response = Buffer.from(data);
 
     // Send the response back to the client
