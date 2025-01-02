cea486c42ec4503d4ae53258f19e4a8d4d3314f8
Sudipta Chowdhury <sudipta.chowdhury@welldev.io>
2025-01-02 14:29:38 +0600

    implemented chunk by chunk buffer sending

--- wago.js
+++ wago.js
@@ -10,9 +10,43 @@ const server = net.createServer((socket) => {
 
     const response = modbus.parseRequest(Buffer.from(data));
 
-    socket.write(response, () => {
-      console.log("Sent Buffer: ", response.toString("hex").toUpperCase());
-    });
+    const sendInBuffer = true;
+
+    if (sendInBuffer) {
+      console.log("Initial Response: ", response);
+
+      const chunkSizes = [2, 3, 4];
+
+      const sendChunk = (offset) => {
+        if (offset < response.length) {
+          const chunkSize = Math.min(
+            response.length - offset,
+            chunkSizes[Math.floor(Math.random() * chunkSizes.length)]
+          );
+
+          const chunks = response.slice(offset, offset + chunkSize);
+
+          setTimeout(() => {
+            socket.write(chunks, () => {
+              console.log(
+                "Sent Buffer: ",
+                chunks.toString("hex").toUpperCase()
+              );
+              sendChunk(offset + chunkSize);
+            });
+          }, 1000);
+        }
+      };
+
+      sendChunk(0);
+    } else {
+      socket.write(response, () => {
+        console.log(
+          "Sent Buffer (one time): ",
+          response.toString("hex").toUpperCase()
+        );
+      });
+    }
   });
 
   socket.on("end", () => {
